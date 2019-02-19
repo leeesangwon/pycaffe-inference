6fc70152032b63661aad3d7f3449dc90a103331c
Sangwon Lee <leee.sangwon@gmail.com>
2019-02-19 18:13:19 +0900

    Add codes to display result image

---
+++ segmentation_infer/icnet_show.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+PROJECT_DIR=$(pwd)
+
+FOLDER_TO_DRAW="frankfurt"
+
+INPUT_FOLDER="./target_image/"$FOLDER_TO_DRAW
+
+LABELMAP_FILE="./colormapcs.mat"
+MODEL_DEF="./model/icnet_cityscapes.prototxt"
+MODEL_WEIGHTS="./model/icnet_cityscapes_trainval_90k.caffemodel"
+
+IMAGE_SIZE="1025 2049"
+
+DOCKER_IMAGE=leeesangwon/nvcaffe:19.01-py2.refinedet.pspnet
+
+xhost +local:docker
+docker run --runtime=nvidia -it --rm \
+    --shm-size=1g \
+    --ulimit memlock=-1 \
+    --ulimit stack=67108864 \
+    -v ${PROJECT_DIR}:/project \
+    -v /tmp/.X11-unix:/tmp/.X11-unix \
+    -e DISPLAY=unix$DISPLAY \
+    ${DOCKER_IMAGE} \
+    bash -c "cd /project && \
+            python segmentation_show.py $INPUT_FOLDER \
+                --labelmap-file $LABELMAP_FILE \
+                --model-def $MODEL_DEF \
+                --model-weights $MODEL_WEIGHTS \
+                --image-size $IMAGE_SIZE"
+xhost -local:docker
--- segmentation_infer/segmentation_show.py
+++ segmentation_infer/segmentation_show.py
@@ -8,24 +8,27 @@ import argparse
 from tqdm import tqdm
 from PIL import Image
 
+import matplotlib.pyplot as plt
+import numpy as np
+
 # suprress Caffe verbose prints
 os.environ['GLOG_minloglevel'] = '2'
 import caffe
 
-from network import DetectionNetwork as Network
+from network import SegmentationNetwork as Network
+
 
 try:
     FileNotFoundError
 except NameError:
     FileNotFoundError = IOError
-
 try:
     FileExistsError
 except NameError:
     FileExistsError = OSError
 
 
-def save_bbox_on_images(input_folder, output_folder, labelmap_file, model_def, model_weights, image_resize, use_center=False):
+def show_seg_of_images(input_folder, labelmap_file, model_def, model_weights, crop_size):
     input_list = glob.glob(os.path.join(input_folder, "*.jpg"))
     input_list += glob.glob(os.path.join(input_folder, "*.JPG"))
     input_list += glob.glob(os.path.join(input_folder, "*.png"))
@@ -34,36 +37,48 @@ def save_bbox_on_images(input_folder, output_folder, labelmap_file, model_def, m
 
     assert input_list, "input_folder(%s) doesn't have image file(jpeg or png format)" % input_folder
 
+    # get canvas size
+    tmp = Image.open(input_list[0])
+    h, w = np.asarray(tmp).shape[0:2]
+    dpi = int(h / 10)
+    figsize = (float(w) / dpi, float(h) / dpi)
+    fig = plt.figure(figsize=figsize, dpi=dpi)
+    fig.show()
+    fig.canvas.draw()
+
     caffe.set_device(0)
     caffe.set_mode_gpu()
-    network = Network(model_def, model_weights, labelmap_file, image_resize)
+    network = Network(model_def, model_weights, labelmap_file, crop_size)
 
     for input_image_path in tqdm(input_list):
-        output_image_path = input_image_path.replace(input_folder, output_folder)
 
         image = caffe.io.load_image(input_image_path)  # np.float32 in range [0, 1]
 
         # inference
-        detections = network(image, use_center)
-        bboxed_img = network.bbox_painter(image, detections)
-        result = Image.fromarray(bboxed_img)
-        result.save(output_image_path)
+        result = network(image)
+        result = 0.7 * image + 0.3 * result
+        result = Image.fromarray((result * 255).astype('uint8'))
+
+        ax = fig.add_subplot(1, 1, 1)
+        ax.imshow(result)
+        ax.axis('off')
+        fig.subplots_adjust(left=0, right=1, top=1, bottom=0, wspace=0, hspace=0)
+        fig.canvas.draw()
+        ax.clear()
+    plt.close(fig)
 
 
 def get_arguments():
     parser = argparse.ArgumentParser()
     parser.add_argument("input_folder", type=str)
-    parser.add_argument("output_folder", type=str)
-    parser.add_argument("--labelmap-file", type=str, default="./labelmap_voc.prototxt",
+    parser.add_argument("--labelmap-file", type=str, default="./labelmap_cs.prototxt",
                         help="path to labelmap file")
-    parser.add_argument("--model-def", type=str, default="./refinedet_deploy.prototxt",
+    parser.add_argument("--model-def", type=str, default="./pspnet_deploy.prototxt",
                         help="path to deploy.prototxt")
-    parser.add_argument("--model-weights", type=str, default="./refinedet.caffemodel",
+    parser.add_argument("--model-weights", type=str, default="./pspnet.caffemodel",
                         help="path to .caffemodel")
-    parser.add_argument("--image-size", type=int, default=320,
+    parser.add_argument("--image-size", nargs='+', type=int, default=473,
                         help="size to resize input images")
-    parser.add_argument("--use-center", action="store_true", 
-                        help="if True, detection is conducted again using center region of input image")
     return parser.parse_args()
 
 
@@ -71,12 +86,13 @@ def main():
     args = get_arguments()
 
     input_folder = args.input_folder
-    output_folder = args.output_folder
     labelmap_file = args.labelmap_file
     model_def = args.model_def
     model_weights = args.model_weights
-    image_resize = args.image_size
-    use_center = args.use_center
+    crop_size = args.image_size
+
+    if len(crop_size) == 1:
+        crop_size = crop_size[0]
 
     if not os.path.isdir(input_folder):
         raise FileNotFoundError("input_folder(%s) does not exist" % input_folder)
@@ -86,22 +102,14 @@ def main():
         raise FileNotFoundError("model_def(%s) does not exist" % model_def)
     if not os.path.isfile(model_weights):
         raise FileNotFoundError("model_weights(%s) does not exist" % model_weights)
-    
-    try:
-        os.makedirs(output_folder)
-    except FileExistsError:
-        pass
 
-    save_bbox_on_images(
+    show_seg_of_images(
         input_folder=input_folder,
-        output_folder=output_folder,
         labelmap_file=labelmap_file,
         model_def=model_def,
         model_weights=model_weights,
-        image_resize=image_resize,
-        use_center=use_center)
+        crop_size=crop_size)
 
 
-if __name__=="__main__":
+if __name__ == "__main__":
     main()
-
